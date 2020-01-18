000887f2c352ef38191d3573d4745903e4c5df54
planetvoodoo <60038309+planetvoodoo@users.noreply.github.com>
2020-01-18 10:04:34 -0500

    Update pvloader.js

--- pvloader.js
+++ pvloader.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-  var scene = document.querySelector('a-scene');
+	var scene = document.querySelector('a-scene');
 	var pvloader = document.querySelector('#pvloader');
-  var model = document.querySelector('#your-model');
+	var model = document.querySelector('#your-model');
 	
 	// HTML5 LOADER
 	window.onload = function() {
 		
-    // ADD VALUE TO BAR ONCE SCENE IS LOADED
+    		// ADD VALUE TO BAR ONCE SCENE IS LOADED
 		scene.addEventListener('loaded', function() {
 			pvloader.value += 50;
 		});
 
-    // ADD VALUE TO BAR ONCE A MODEL IS LOADED
+    		// ADD VALUE TO BAR ONCE A MODEL IS LOADED
 		model.addEventListener('model-loaded', function() {
 			pvloader.value += 50;
 		});
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			loading.style.display = 'none';
 		}
 
-    // TIMER TO CHECK IF VALUE IS 100% EVERY 1 SECOND
+    		// TIMER TO CHECK IF VALUE IS 100% EVERY 1 SECOND
 		var checkStatus = setInterval(myTimer, 1000);
 		
 		function myTimer() {
@@ -32,4 +32,4 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		}
 
-	}
+}
